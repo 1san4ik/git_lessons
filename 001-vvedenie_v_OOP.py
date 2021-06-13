0cfa177f3cee8cb1891d9d120cd19eb3b5fd887e
1san4k <1san4ik@gmail.com>
2021-06-13 19:06:04 +0300

    Assignments 2 and 3

--- 001-vvedenie_v_OOP.py
+++ 001-vvedenie_v_OOP.py
@@ -1,28 +1,137 @@
-class Book:
-    def __init__(self, autor, name_book, year, ganre):
-        self.autor = autor
-        self.name_book = name_book
-        self.year = year
-        self.ganre = ganre
+# # 1. Задание 1
+# # Создайте класс, описывающий книгу. Он должен содержать информацию об авторе, названии, годе
+# # издания и жанре. Создайте несколько разных книг. Определите для него операции проверки на
+# # равенство и неравенство, методы __repr__ и __str__.
+#
+# class Book:
+#     def __init__(self, autor, name_book, year, ganre):
+#         self.autor = autor
+#         self.name_book = name_book
+#         self.year = year
+#         self.ganre = ganre
+#
+#     def __repr__(self):
+#         return f"Book (autor: {self.autor}, name: {self.name_book}, year: {self.year}, ganre: {self.ganre})"
+#
+#     def __str__(self):
+#         return f"Book (autor: {self.autor}, name: {self.name_book}, year: {self.year}, ganre: {self.ganre})"
+#
+#     def __eq__(self, other):
+#         if not isinstance(other, Book):
+#             return False
+#         return self.autor == other.autor and self.name_book == other.name_book and self.year == other.year \
+#             and self.ganre == other.ganre
+#
+#
+# book_1 = Book("Andrzej Sapkowski",
+#               "The Witcher",
+#               "2016",
+#               "fantasy")
+# book_2 = Book("Leo Tolstoy",
+#               "War and Peace",
+#               "1867",
+#               "historical novel")
+# book_3 = Book("George Orwell",
+#               "1984",
+#               "1949",
+#               "dystopia")
+# book_1_copy = Book("Andrzej Sapkowski",
+#                    "The Witcher",
+#                    "2016",
+#                    "fantasy")
+#
+# print(repr(book_1))
+# print(str(book_2))
+# print(book_1 == book_1_copy)
+# print("*" * 60)
+#
+# # 2. Задание 2
+# # Создайте класс, описывающий отзыв к книге. Добавьте в класс книги поле – список отзывов. Сделайте
+# # так, что при выводе книги на экран при помощи функции print также будут выводиться отзывы к ней.
+#
+# class Books:
+#     def __init__(self, autor, name_book, year, ganre):
+#         self.autor = autor
+#         self.name_book = name_book
+#         self.year = year
+#         self.ganre = ganre
+#         self.review = []
+#
+#     def __str__(self):
+#         line = f"Книга: '{self.name_book}' автор: {self.autor}, получила отзыв:\n"
+#         for i in self.review:
+#             line +=f"Отзыв: {str(i)} \n"
+#         return line
+#
+#     def __eq__(self, other):
+#         if not isinstance(other, Book):
+#             return False
+#         return self.autor == other.autor and self.name_book == other.name_book and self.year == other.year \
+#             and self.ganre == other.ganre
+#
+#     def add_review(self, review_text, user):
+#         rev = Review(review_text, user)
+#         self.review.append(rev)
+#
+#
+# class Review:
+#     def __init__(self, review_text, user):
+#         self.review_text = review_text
+#         self.user = user
+#
+#     def __str__(self):
+#         return f"Пользователь {self.user}, оставил отзыв: '{self.review_text}'"
+#
+#
+# book_4 = Books("Andrzej Sapkowski",
+#               "The Witcher",
+#               "2016",
+#               "fantasy")
+# book_5 = Books("Leo Tolstoy",
+#               "War and Peace",
+#               "1867",
+#               "historical novel")
+# book_6 = Books("George Orwell",
+#               "1984",
+#               "1949",
+#               "dystopia")
+#
+# rev_1 = input("Оставьте отзыв на книгу: ")
+# rev_2 = input("Напишите свое имя: ")
+# book_4.add_review(rev_1, rev_2)
+# print(book_4)
+# print("*" * 60)
+#
+# # Задание 3
+# # Используя ссылки в конце данного урока, ознакомьтесь с таким средством инкапсуляции как свойства.
+# # Ознакомьтесь с декоратором property в Python. Создайте класс, описывающий температуру и
+# # позволяющий задавать и получать температуру по шкале Цельсия и Фаренгейта, причём данные могут
+# # быть заданы в одной шкале, а получены в другой.
+#
+# class Celsius:
+#     def __init__(self, temperature=0):
+#         self.temperature = temperature
+#
+#     def to_fahrenheit(self):
+#         return (self.temperature * 1.8) + 32
+#
+#     @property
+#     def temperature(self):
+#         return self._temperature
+#
+#     @temperature.setter
+#     def temperature(self, value):
+#         if value < -273.15:
+#             raise ValueError("Температура ниже -273 невозможна")
+#         self._temperature = value
+#
+#
+# human = Celsius(int(input("Введите температуру: ")))
+# print(f"Температура по Цельсию: {human.temperature}")
+# print(f"Температура по Фаренгейту: {round(human.to_fahrenheit(), 2)}")
+#
+# Дополнительное задание
+# Задание
+# Создайте класс, описывающий автомобиль. Создайте класс автосалона, содержащий в себе список
+# автомобилей, доступных для продажи, и функцию продажи заданного автомобиля.
 
-    def __repr__(self):
-        return f"Book (autor: {self.autor}, name: {self.name_book}, year: {self.year}, ganre: {self.ganre})"
-
-    def __str__(self):
-        return f"Book (autor: {self.autor}, name: {self.name_book}, year: {self.year}, ganre: {self.ganre})"
-
-    def __eq__(self, other):
-        if not isinstance(self, other):
-            return False
-        return self.autor == other.autor and self.name_book == other.name_book and self.year == other.year \
-    and self.ganre == other.ganre
-
-
-book_1 = Book(autor="Andrzej Sapkowski", name_book="The Witcher", year="2016", ganre="fantasy")
-book_2 = Book(autor="Leo Tolstoy", name_book="War and Peace", year="1867", ganre="historical novel")
-book_3 = Book(autor="George Orwell", name_book="1984", year="1949", ganre="dystopia")
-book_1_copy = Book(autor="Andrzej Sapkowski", name_book="The Witcher", year="2016", ganre="fantasy")
-
-print(book_1.__repr__())
-print(book_1.__str__())
-print(book_1 == book_2)
